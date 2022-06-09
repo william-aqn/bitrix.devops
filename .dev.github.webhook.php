44b4d083ce6027a1b6454e1d420ea763b3c585ed
william-aqn <dark.crimson.d.c.r.m@gmail.com>
2022-06-09 15:34:42 +0300

    Update .dev.github.webhook.php

--- .dev.github.webhook.php
+++ .dev.github.webhook.php
@@ -3,6 +3,6 @@ define("TOKEN", "TOKEN");
 $json = json_decode(file_get_contents("php://input"), true);
 $ref = str_replace('refs/heads/', '', $json['ref']);
 $ref = preg_replace("[^0-9a-zA-Z\-]", '', $ref);
-$command = exec("dev.sh $ref" . $script, $console);
+$command = exec("dev.sh $ref", $console);
 echo $console;
 die();
