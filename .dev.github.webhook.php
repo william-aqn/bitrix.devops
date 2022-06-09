86831d94b320f7196ea779653927b575aee780e0
william-aqn <dark.crimson.d.c.r.m@gmail.com>
2022-06-09 15:58:38 +0300

    Update .dev.github.webhook.php

--- .dev.github.webhook.php
+++ .dev.github.webhook.php
@@ -1,8 +1,17 @@
 <?php
+
 define("TOKEN", "TOKEN");
-$json = json_decode(file_get_contents("php://input"), true);
+$content = file_get_contents("php://input");
+$signature = hash_hmac('sha1', $content, TOKEN);
+
+if ($_SERVER['HTTP_X_HUB_SIGNATURE'] != $signature) {
+    echo "err sig";
+    die();
+}
+
+$json = json_decode($content, true);
 $ref = str_replace('refs/heads/', '', $json['ref']);
 $ref = preg_replace("[^0-9a-zA-Z\-]", '', $ref);
-$command = exec("dev.sh $ref", $console);
+$command = exec("dev.sh $ref" . $script, $console);
 echo $console;
 die();
